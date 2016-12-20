0092bd9a6fee7ac26a418749bcea950950e1bc9e
jotaemece <jotaemece2110@gmail.com>
2016-12-20 20:12:51 +0100

    13. Impulsor nave

---
+++ Assets/Scripts/ImpulsorNave.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ImpulsorNave : MonoBehaviour {
+
+	public float velocidadMovimiento;
+
+	void Start () {
+
+
+	}
+
+
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.UpArrow))
+		{
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (velocidadMovimiento, GetComponent<Rigidbody2D> ().velocity.x);
+		}
+			
+	}
+}
+
+
