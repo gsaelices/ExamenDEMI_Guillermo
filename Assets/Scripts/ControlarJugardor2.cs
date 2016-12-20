756e28a0cec61ebeadebb777b463bdcc39fa8696
jotaemece <jotaemece2110@gmail.com>
2016-12-20 19:40:00 +0100

    4. Movimiento Jugador 2

---
+++ Assets/Scripts/ControlarJugardor2.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControlarJugardor2 : MonoBehaviour {
+	
+	public float velocidadMovimiento;
+
+	void Start () {
+
+
+	}
+
+
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.W))
+		{
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (velocidadMovimiento, GetComponent<Rigidbody2D> ().velocity.x);
+		}
+
+		if (Input.GetKeyDown (KeyCode.S))
+		{
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (-velocidadMovimiento, GetComponent<Rigidbody2D> ().velocity.x);
+		}
+
+
+		if (Input.GetKeyDown (KeyCode.D))
+		{
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (velocidadMovimiento, GetComponent<Rigidbody2D> ().velocity.y);
+		}
+
+		if (Input.GetKeyDown (KeyCode.A))
+		{
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (-velocidadMovimiento, GetComponent<Rigidbody2D> ().velocity.y);
+		}
+	}
+}
+
+
+
